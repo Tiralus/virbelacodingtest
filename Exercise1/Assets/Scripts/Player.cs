c6abbdeed4e397275efbe9c0ffd1dfadeee143d6
Michael Picerno <mpicerno@makermischief.com>
2020-11-22 11:04:26 -0800

    Phase 1
    Initial commit for phase 1 functional requirements

---
+++ Exercise1/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿namespace Game.Player
+{
+    using Game.Thing;
+    
+    using System.Collections;
+    using System.Collections.Generic;
+    
+    using UnityEngine;
+
+    public class Player : MonoBehaviour
+    {
+        // Set through inspector this is the list of thing objects
+        public List<Thing> things;
+
+        private Thing closestThing;
+
+        // Update is called once per frame
+        void Update()
+        {
+            float distanceToClosest = Mathf.Infinity;
+            foreach (Thing thing in things)
+            {
+                float distanceToObject = Vector3.Distance(this.transform.position, thing.transform.position);
+
+                if (distanceToObject < distanceToClosest)
+                {
+                    if (closestThing)
+                        closestThing.SetHighLightOff();
+                    
+                    thing.SetHighlightOn();
+                    closestThing = thing;
+                    distanceToClosest = distanceToObject;
+                }
+            }
+            if (closestThing)
+                Debug.DrawLine(this.transform.position, closestThing.transform.position);
+        }
+    }
+}
+
+
---
+++ Exercise1/Assets/Scripts/Thing.cs
@@ -0,0 +1,34 @@
+﻿namespace Game.Thing
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Thing : MonoBehaviour
+    {
+        private Material material;
+
+        private Color highLightColor = Color.red; 
+        
+        private Color defaultColor = Color.white;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            material = GetComponent<Renderer>().material;
+            SetHighLightOff();
+        }
+
+        public void SetHighlightOn()
+        {
+            material.color = highLightColor;
+        }
+
+        public void SetHighLightOff()
+        {
+            material.color = defaultColor;
+        }
+    }
+}
+
+
