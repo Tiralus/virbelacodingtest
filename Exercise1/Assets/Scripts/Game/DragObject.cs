252a9f62794a7452b40461c658b269761c366061
Michael Picerno <mpicerno@makermischief.com>
2020-11-23 14:41:36 -0800

    Fix warning
    Warning for hiding camera

--- Exercise1/Assets/Scripts/Game/DragObject.cs
+++ Exercise1/Assets/Scripts/Game/DragObject.cs
@@ -7,25 +7,25 @@
         private Vector3 screenPoint;
         private Vector3 offset;
 
-        private Camera camera;
+        private Camera cam;
 
         private void Start()
         {
-            camera = Camera.main;
+            cam = Camera.main;
         }
 
         void OnMouseDown()
         {
-            screenPoint = camera.WorldToScreenPoint(gameObject.transform.position);
+            screenPoint = cam.WorldToScreenPoint(gameObject.transform.position);
             offset = gameObject.transform.position -
-                     camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+                     cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
         }
 
         void OnMouseDrag()
         {
             Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 
-            Vector3 curPosition = camera.ScreenToWorldPoint(curScreenPoint) + offset;
+            Vector3 curPosition = cam.ScreenToWorldPoint(curScreenPoint) + offset;
             transform.position = curPosition;
         }
     }
